4f97b3c49e48a7eb500a3d4697be35dd61c19ef1
Constantin Ungureanu <constantin.ungureanu-conu@asml.com>
2025-01-22 17:59:12 +0100

    Updated logs.

--- src/main/java/org/acme/JwtResource.java
+++ src/main/java/org/acme/JwtResource.java
@@ -1,21 +1,26 @@
 package org.acme;
 
 import jakarta.annotation.security.RolesAllowed;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.jboss.logging.Logger;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 @Path("/api")
 @RolesAllowed("**")
 public class JwtResource {
 
-    @Inject
-    JsonWebToken accessToken;
+    private static final Logger LOG = Logger.getLogger(JwtResource.class);
+
+    public JwtResource(JsonWebToken idToken) {
+        this.idToken = idToken;
+    }
+
+    private final JsonWebToken idToken;
 
     @WithSpan
     @Path("/user")
@@ -23,6 +28,7 @@ public class JwtResource {
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed({ "user", "admin" })
     public Response user() {
+        LOG.info("/user accessed by user " + idToken.getName());
         return Response.ok().build();
     }
 
@@ -32,6 +38,7 @@ public class JwtResource {
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("admin")
     public Response admin() {
+        LOG.info("/admin accessed by user " + idToken.getName());
         return Response.ok().build();
     }
 }
